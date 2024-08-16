8471b9cfef82be376000ed96dbbb60df61dbbf11
starksailor <starksailorgame@gmail.com>
2024-08-16 11:10:03 +0700

    Update

--- scripts/auth.sh
+++ scripts/auth.sh
@@ -6,8 +6,8 @@ export RPC_URL="http://localhost:5050"
 
 # export WORLD_ADDRESS=$(cat ./manifests/dev/manifest.json | jq -r '.world.address')
 # export ACTION_ADDRESS=$(cat ./manifests/dev/manifest.json | jq -r '.contracts[] | select(.name == "dragark_2::systems::actions::actions" ).address')
-export WORLD_ADDRESS="0x14257d06fbd345c5d2b4fca7b49a24a7f3f7f2e4fcdd47b3867b6e31af42b38"
-export ACTION_ADDRESS="0xd0ff27f41260eead8d84704a14dfb273f2a46f88a6cd71b2e6a33c7bebe095"
+export WORLD_ADDRESS="0xabcd"
+export ACTION_ADDRESS="0xabcd"
 
 echo "---------------------------------------------------------------------------"
 echo world : $WORLD_ADDRESS
